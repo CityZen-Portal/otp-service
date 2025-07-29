2bacc0b32d61597d73e0c2d5eebe60c20d74cf3d
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-07-30 00:57:55 +0530

    fix:changed to hosted client

--- src/main/java/com/cityzen/auth/clients/UserInterface.java
+++ src/main/java/com/cityzen/auth/clients/UserInterface.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-//@FeignClient(name = "UserManagementService", url = "https://auth-backend-cpcr.onrender.com")
-@FeignClient(name = "UserManagementService", url = "http://localhost:7000")
+@FeignClient(name = "UserManagementService", url = "https://auth-backend-cpcr.onrender.com")
+//@FeignClient(name = "UserManagementService", url = "http://localhost:7000")
 public interface UserInterface {
     @GetMapping("/api/auth/validate")
      ResponseEntity<TokenResponseDto> validateUser(@RequestHeader("token")  String token);
