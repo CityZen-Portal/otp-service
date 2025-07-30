4710278df6a5fa48c7fedb2912c4fcac7a30cf55
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-07-30 14:52:10 +0530

    fix:changed the host link

--- src/main/java/com/cityzen/auth/clients/UserInterface.java
+++ src/main/java/com/cityzen/auth/clients/UserInterface.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "UserManagementService", url = "https://auth-backend-cpcr.onrender.com")
+@FeignClient(name = "UserManagementService", url = "https://auth-backend-obcu.onrender.com")
 public interface UserInterface {
     @GetMapping("/api/auth/validate")
      ResponseEntity<TokenResponseDto> validateUser(@RequestHeader("token")  String token);
